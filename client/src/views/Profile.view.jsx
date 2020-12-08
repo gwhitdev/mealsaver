d9cced4b8e8ec805307434988a3ada8055f0e2fe
Gareth Whitley <me@garethwhitley.online>
2020-12-08 20:53:17 +0000

    added image

--- client/src/views/Profile.view.jsx
+++ client/src/views/Profile.view.jsx
@@ -4,27 +4,33 @@ import { connect } from 'react-redux';
 import { useAuth0 } from '@auth0/auth0-react';
 
 import { GET_INGREDIENTS } from '../redux/ingredients/ingredientsActions';
-import { Card, Grid, Icon, Container, Image, Header } from 'semantic-ui-react';
+import { Grid, Container, Loader, Image, Dimmer, Card } from 'semantic-ui-react';
 
 import MealSuggestion from '../components/MealSuggestion/MealSuggesstion.component';
+import WhiteImage from '../white-image.png';
 
 const Profile = ({ ingredients, GET_INGREDIENTS }) => {
     const { user } = useAuth0();
     const [recipeInfo, setRecipeInfo] = useState({});
+    const [loading, setLoading] = useState(false);
+
+    const findRecipe = async (ingredients) => {
+        const chosenIngredient = ingredients[0].name;
+        setLoading(true);
+        await axios.get(`https://api.spoonacular.com/recipes/findByIngredients?ingredients=${chosenIngredient}&number=1&apiKey=0e11a1a68fe54e18bfe7ab69066ec1ef`)
+        .then(res => res.data[0].id)
+        .then(id => axios.get(`https://api.spoonacular.com/recipes/${id}/information?apiKey=0e11a1a68fe54e18bfe7ab69066ec1ef`))
+            .then(res => {setRecipeInfo(res.data); setLoading(false)});
+    };
+
+   
 
     useEffect(() => {
-        const findRecipe = async (ingredients) => {
-            const chosenIngredient = ingredients[0].name;
-            await axios.get(`https://api.spoonacular.com/recipes/findByIngredients?ingredients=${chosenIngredient}&number=1&apiKey=0e11a1a68fe54e18bfe7ab69066ec1ef`)
-            .then(res => res.data[0].id)
-            .then(id => axios.get(`https://api.spoonacular.com/recipes/${id}/information?apiKey=0e11a1a68fe54e18bfe7ab69066ec1ef`))
-                .then(res => setRecipeInfo(res.data));
-        };
-
-        
-
-        ingredients.length === 0 ? GET_INGREDIENTS(user.sub) : findRecipe(ingredients);
-    }, [user, GET_INGREDIENTS, ingredients])
+        function getIngredients(ingredients, user){
+            ingredients.length === 0 ? GET_INGREDIENTS(user.sub) : findRecipe(ingredients);
+        }
+        getIngredients(ingredients, user)},
+    [GET_INGREDIENTS, user, ingredients, ingredients.length, user.sub]);
 
     return (
         
@@ -36,7 +42,10 @@ const Profile = ({ ingredients, GET_INGREDIENTS }) => {
             </Grid.Column>    
             
             <Grid.Column>
-                <MealSuggestion meal={recipeInfo} />
+                { loading 
+                    ? <Card><Card.Content><Card.Description><Dimmer active inverted ><Loader inverted inline> Getting today's recipe </Loader></Dimmer><Image src={WhiteImage} /></Card.Description></Card.Content></Card>
+                    : <MealSuggestion loading={loading} meal={recipeInfo} />
+                }
             </Grid.Column>
             
             

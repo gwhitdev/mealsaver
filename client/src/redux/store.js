83764c632e93b1baf598b38dec6ecf5f9080a1c9
Gareth Whitley <me@garethwhitley.online>
2020-12-08 21:07:01 +0000

    removed devtools

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -7,5 +7,5 @@ const middlewares = [thunk,logger];
 
 export const store = createStore(
     rootReducer,
-    compose(applyMiddleware(...middlewares), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+    compose(applyMiddleware(...middlewares), /*window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()*/)
 );
